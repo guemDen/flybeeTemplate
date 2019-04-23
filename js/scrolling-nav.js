ecede0ced6f2a7c452e3c2d7d8c0ef607cd2e889
guemDen <guemues_deniz@hotmail.de>
2019-04-23 23:01:50 +0200

    added slider and shop elements

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,6 +1,12 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  // Carousel functions
+  $('.Products').carousel({
+    interval: 1000,
+    pause: "false"
+  });
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
