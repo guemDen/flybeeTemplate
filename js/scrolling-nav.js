4bc00ee5c9f4f53cae73f05e2fd119f2437d4d6b
guemDen <guemues_deniz@hotmail.de>
2019-05-09 04:05:10 +0200

    cards and product description are added

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -15,6 +15,12 @@
     $("#carouselProductsNext").carousel("next");
   });
 */
+
+  $('#color-list a').on('click', function(e) {
+    e.preventDefault()
+    $(this).tab('show')
+  });
+  
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
